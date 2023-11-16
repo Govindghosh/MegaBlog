c3f85be3ca7289c09b82f825644dcd8294310b40
Govind Ghosh <130273101+Govindghosh@users.noreply.github.com>
2023-11-16 13:30:17 +0530

    only left routing

--- src/components/post-form/Postform.jsx
+++ src/components/post-form/Postform.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import {useForm} from 'react-hook-form'
 import {Button, Input, RTE, Select} from '../index'
 import appwriteService from '../../appwrite/configuration'
@@ -37,15 +37,86 @@ function Postform({post}) {
                     if (file) {
                        const fileId =  file.$id
                        data.FeaturedImage=fileId
-                       await //here you go....
+                      const dbPost= await appwriteService.createPost({...data,
+                    userID:userData.$id,
+                    })
+                    if (dbPost) {
+                        navigate(`/post/${dbPost.$id}`)
+                    }
                     }
                 }
 
             }
+            const slugTransfer = useCallback((value)=>{
+                if (value && typeof value === 'string') {
+                    return value.trim().toLowerCase().replace(/[^a-zA-Z\d\s]+/g, "-")
+                    .replace(/\s/g, "-");
+                        return ''
+
+                }
+            },[])
+            useEffect(() => {
+             const subscription = watch((value, {name})=>{
+                if (name === 'title') {
+                    setValue('slug', slugTransfer(value.title, {
+                        shouldValidate: true
+                    }))
+                    
+                }
+             })
+             return ()=>{
+                subscription.unsubscribe()
+             }
+            }, [watch, slugTransfer, setValue])
+            
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
+                />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+            </div>
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
   )
 }
 
